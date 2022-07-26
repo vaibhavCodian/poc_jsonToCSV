67d5cef0d6cfcb21c56d9ecdf7b7e78a7a051069
Vaibhav <vaibhavshukla.try@gmail.com>
2022-07-26 18:20:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import json
 import pandas as pd
 
-data = json.load(open("response.json"))
-data["users"]
 
-users = pd.json_normalize(data, "users")
-companies = pd.json_normalize(data, "companies")
+def generateCSV(name):
+    data = json.load(open(name + ".json"))
+    classes = []
+    details = []
 
-df = pd.concat([users, companies], keys=["users", "companies"],)
-df.to_csv("res.csv")
+    for key, value in data.items():
+        print("class: ", key)
+        classes.append(key)
+
+    for class_ in classes:
+        for key, value in data[class_].items():
+            print("details_for: ", key)
+            details.append(value)
+
+    df = pd.json_normalize(details)
+    df.to_csv(name + ".csv")
+    print(f'\n \t CSV For json {name} Generated... \n')
+
+
+generateCSV("sample-1")
+generateCSV("sample-2")
